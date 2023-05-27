5a72bfe229ed667b8c0e772b044e619619d48fc0
Púček, Radovan <pucekrad@fel.cvut.cz>
2023-05-27 11:56:56 +0200

    change

--- src/main/java/test/StalemateTest.java
+++ src/main/java/test/StalemateTest.java
@@ -1,9 +1,15 @@
 package test;
 
 import MVC.ManagerUI;
+import MVC.GameUI;
 import MVC.Operator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
 
 import static java.lang.Thread.sleep;
 
@@ -25,9 +31,26 @@ public class StalemateTest {
         op.ui.game.redrawBackground();
         op.ui.op.window.repaint();
 
+        simulateClick();
+
+
+
+
 
         sleep(5000);
     }
 
 
+    public void simulateClick(int x, int y) {
+        JLabel label = op.ui.game.tiles[x][y]
+        try {
+            Robot robot = new Robot();
+            Point point = label.getLocationOnScreen();
+            robot.mouseMove(point.x + label.getWidth() / 2, point.y + label.getHeight() / 2);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        } catch (AWTException ex) {
+            ex.printStackTrace();
+        }
+    }
 }

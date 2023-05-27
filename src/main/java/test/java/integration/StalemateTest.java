651041ec0268800079a1b4a9aab84a81767bcbdc
Púček, Radovan <pucekrad@fel.cvut.cz>
2023-05-27 14:01:08 +0200

    stalemate stalemate StalemateTest.java finalized

--- src/main/java/test/java/integration/StalemateTest.java
+++ src/main/java/test/java/integration/StalemateTest.java
@@ -21,68 +21,30 @@ class StalemateTest {
     @Test
     void testStalemateRepeating() throws InterruptedException {
 
-        isTurnOf('W');
-        clicker.simulateTileClick(7,1);
-        clicker.simulateTileClick(5,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 7,1,5,2,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(0,1);
-        clicker.simulateTileClick(2,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('B', 0,1,2,2,-1);
 
-        isTurnOf('W');
-        clicker.simulateTileClick(5,2);
-        clicker.simulateTileClick(7,1);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 5,2,7,1,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(2,2);
-        clicker.simulateTileClick(0,1);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('B', 2,2,0,1,-1);
 
-        isTurnOf('W');
-        clicker.simulateTileClick(7,1);
-        clicker.simulateTileClick(5,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 7,1,5,2,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(0,1);
-        clicker.simulateTileClick(2,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('B', 0,1,2,2,-1);
 
-        isTurnOf('W');
-        clicker.simulateTileClick(5,2);
-        clicker.simulateTileClick(7,1);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 5,2,7,1,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(2,2);
-        clicker.simulateTileClick(0,1);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('B', 2,2,0,1,-1);
 
-        isTurnOf('W');
-        clicker.simulateTileClick(7,1);
-        clicker.simulateTileClick(5,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 7,1,5,2,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(0,1);
-        clicker.simulateTileClick(2,2);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('B', 0,1,2,2,-1);
 
-        isTurnOf('W');
-        clicker.simulateTileClick(5,2);
-        clicker.simulateTileClick(7,1);
-        Assertions.assertEquals(-1, clicker.op.ui.game.board.pgn.end);
+        move('W', 5,2,7,1,-1);
 
-        isTurnOf('B');
-        clicker.simulateTileClick(2,2);
-        clicker.simulateTileClick(0,1);
-        Assertions.assertEquals(2, clicker.op.ui.game.board.pgn.end);
+        move('B', 2,2,0,1,2);
 
-        sleep(5000);
-        System.out.println(clicker.op.ui.game.board.isPat());
     }
 
     @Test
@@ -196,30 +158,136 @@ class StalemateTest {
         move('W', 0,7,0,1,-1);
         move('B', 1,1,0,1,2);
 
+    }
 
+    @Test
+    public void testStatelamateOnlyKnight() throws InterruptedException {
+        for (int i = 0; i < 8; i-=-1) {
+            move('W', 6, i, 4, i, -1);
+            move('B', 1, i, 3, i, -1);
+        }
+        for (int i = 0; i < 8; i+=2) {
+            move('W', 4, i, 3, i+1, -1);
+            move('B', 3, i, 4, i+1, -1);
+        }
 
-        sleep(1000000);
+        move('W', 7,3,4,3,-1);
+        move('B', 0,3,3,3,-1);
+
+        move('W', 4,3,4,1, -1);
+        move('B', 3,3,3,1, -1);
+
+        move('W', 4,1,4,5,-1);
+        move('B', 3,1,3,5,-1);
+
+        move('W', 4,5,4,7, -1);
+        move('B', 3,5,3,7,-1);
+
+        move('W', 7,0,0,0,-1);
+        move('B', 3,7,3,1,-1);
+
+        move('W', 0,0,0,1,-1);
+        move('B', 3,1,7,1,-1);
+
+        move('W', 0,1,0,2,-1);
+        move('B', 0,4,1,3,-1);
+
+        move('W', 0,2,0,5,-1);
+        move('B', 7,1,7,2,-1);
+
+        move('W', 7,4,6,4,-1);
+        move('B', 7,2,2,2,-1);
+
+        move('W', 0,5,0,6,-1);
+        move('B', 2,2,7,7,-1);
+
+        move('W', 0,6,0,7,-1);
+        move('B', 7,7,4,7,-1);
+
+        move('W', 0,7,4,7,-1);
+        move('B', 1,3,2,2,-1);
+
+        move('W', 6,4,7,4,-1);
+        move('B', 2,2,3,2,-1);
+
+        move('W', 7,5,3,1,-1);
+        move('B', 3,2,3,1,-1);
+
+        move('W', 4,7,4,1,-1);
+        move('B', 3,1,4,1,2);
+
+    }
+
+    @Test
+    public void testStalemateNoMoves() throws InterruptedException {
+        for (int i = 0; i < 8; i-=-1) {
+            move('W', 6, i, 4, i, -1);
+            move('B', 1, i, 3, i, -1);
+        }
+        for (int i = 0; i < 8; i+=2) {
+            move('W', 4, i, 3, i+1, -1);
+            move('B', 3, i, 4, i+1, -1);
+        }
+
+        move('W', 7,3,4,3,-1);
+        move('B', 0,3,3,3,-1);
+
+        move('W', 4,3,4,1, -1);
+        move('B', 3,3,3,1, -1);
+
+        move('W', 4,1,4,5,-1);
+        move('B', 3,1,3,5,-1);
+
+        move('W', 4,5,4,7, -1);
+        move('B', 3,5,3,7,-1);
+
+        move('W', 7,0,0,0,-1);
+        move('B', 3,7,3,1,-1);
+
+        move('W', 0,0,0,1,-1);
+        move('B', 3,1,7,1,-1);
+
+        move('W', 0,1,0,2,-1);
+        move('B', 0,4,1,3,-1);
+
+        move('W', 0,2,0,5,-1);
+        move('B', 7,1,7,2,-1);
+
+        move('W', 7,4,6,4,-1);
+        move('B', 7,2,2,2,-1);
+
+        move('W', 0,5,0,6,-1);
+        move('B', 2,2,7,7,-1);
+
+        move('W', 0,6,0,7,-1);
+        move('B', 7,7,5,7,-1);
+
+        move('W', 7,6,5,7,-1);
+        move('B', 1,3,1,2,-1);
+
+        move('W', 4,7,4,2,-1);
+        move('B', 1,2,1,1,-1);
+
+        move('W', 4,2,2,2,-1);
+        move('B', 1,1,1,0,-1);
+
+        move('W', 5,7,3,6,2);
 
     }
 
     public void isTurnOf(char expected) {
-        try {
-            sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         Assertions.assertEquals(expected, clicker.op.ui.game.board.turnOf);
     }
 
     public void move(char turnOf, int fromX, int fromY, int toX, int toY, int gameState) {
-//        isTurnOf(turnOf);
-//        try {
-//            sleep(500);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        isTurnOf(turnOf);
         clicker.simulateTileClick(fromX,fromY);
         clicker.simulateTileClick(toX,toY);
+        try {
+            sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Assertions.assertEquals(gameState, clicker.op.ui.game.board.pgn.end);
     }
 
